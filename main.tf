647bbb6009ac397f02e93422de12779f9ca47c23
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-30 16:32:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"  # Old version for testing
-    }
-  }
-}
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "rg-test-renovate"
-  location = "West Europe"
-}
+terraform { 
+  required_providers { 
+    azurerm = { 
+      source  = "hashicorp/azurerm" 
+      version = "=3.84.0" 
+    } 
+  } 
+} 
