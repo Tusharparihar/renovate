267907612a52c1e6e9f8ca54b0fd810f3242ea25
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-10-01 13:18:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
-terraform { 
-  required_providers { 
-    azurerm = { 
-      source  = "hashicorp/azurerm" 
-      version = "=3.84.0" 
-    } 
-  } 
-} 
+terraform {
+  required_version = "~> 1.0.0" # Older core version
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.40.0" # Older provider version
+    }
+  }
+}
+
+# You don't need to define resources, just the provider block
+provider "azurerm" {
+  features {}
+}
